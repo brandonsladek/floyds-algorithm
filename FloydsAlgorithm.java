b03cb4cb77c088e47812095a3ac9ef5b6bb3114b
brandonsladek <brandonsladek@gmail.com>
2015-04-07 00:00:59 -0600

    Added important note, made looping edge weights 0

--- FloydsAlgorithm.java
+++ FloydsAlgorithm.java
@@ -29,6 +29,14 @@ public class Main {
 			}
 		}
 		
+		// Looping edges should have 0 weight
+		for (int a = 0; a < 5; a++) {
+			for (int b = 0; b < 5; b++) {
+				if (a == b) 
+					graph[a][b] = 0;
+			}
+		}
+		
 		// Variables for x and y axes in matrix
 				int i = 0;
 				int j = 0;
@@ -49,8 +57,8 @@ public class Main {
 				}
 			}
 		} // End while loop
-				
-// TEST VALUES	
+					
+// 		   TEST VALUES	
 //		graph[0][1] = 50;
 //		graph[0][3] = 80;
 //		graph[1][2] = 60;
@@ -59,6 +67,19 @@ public class Main {
 //		graph[3][2] = 20;
 //		graph[3][4] = 70;
 //		graph[4][1] = 50;
+		
+		// Tell user important note
+		System.out.println("NOTE: This program displays an");
+		System.out.println("edge weight of 0 at all looping");
+		System.out.println("edges, since there is no need to");
+		System.out.println("travel to and from the same edge.");
+		System.out.println("But don't worry, the Floyd's");
+		System.out.println("Algorithm code does not consider");
+		System.out.println("looping edges as it traverses the");
+		System.out.println("adjacency matrix of the graph,");
+		System.out.println("meaning all of the shortest paths");
+		System.out.println("are still computed correctly.");
+		System.out.println();
 		
 		// Display initial graph
 		System.out.println("INITIAL ADJACENCY MATRIX:");
@@ -69,9 +90,14 @@ public class Main {
 		floydsAlgorithm(graph);
 		
 		// Display final graph
-		System.out.println("AFTER FLOYD'S ALGORITHM:");
+		System.out.println("AFTER FLOYD'S ALGORITHM...\n");
+		System.out.println("FINAL ADJACENCY MATRIX:");
 		System.out.println();
 		displayGraph(graph);
+		
+		// Exit with good manners
+		System.out.println();
+		System.out.println("Goodbye...");
 		
 	} // End of main method
 	
